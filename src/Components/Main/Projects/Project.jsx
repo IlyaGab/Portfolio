604b871f271843a30eec72f9e323ea2f05f6be80
IlyaGab <ilyagab1994@gmail.com>
2022-06-06 22:57:54 +0300

    add @media to portfolio

--- src/Components/Main/Projects/Project.jsx
+++ src/Components/Main/Projects/Project.jsx
@@ -4,12 +4,10 @@ import classes from './Project.module.css'
 const Project = () => {
     return (
         <div className={classes.projectsMainBlock}>
-            <div className={classes.projectsImageBlock}>
-                <span className={classes.projectImage}>Картинка</span>
                 <div className={classes.projectButtonBlock}>
                     <button>Смотреть</button>
                 </div>
-            </div>
+
             <div className={classes.projectsDescription}>
                 <span className={classes.projectName}>Название проекта</span>
                 <span className={classes.projectShortDescription}>Краткое описание</span>
