d99c7cda5a380dc5364feb940c533d42024ddee5
IlyaGab <ilyagab1994@gmail.com>
2022-07-19 16:17:56 +0300

    refactor main component styles

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import classes from './Header.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { faHouzz, faStickerMule } from '@fortawesome/free-brands-svg-icons';
+import { faHospital } from '@fortawesome/free-regular-svg-icons';
 
 const Header = () => {
     return (
         <div className={classes.headerWrapper}>
                 <div className={classes.navItems}>
-                    <NavLink to={'/'} className={classes.listItem}>Main</NavLink>
-                    <NavLink to={'/skills'} className={classes.listItem}>My Skills</NavLink>
+                    <NavLink to={'/'} className={classes.listItem}>
+                        <FontAwesomeIcon icon={faHospital} />
+                        <h2>Home</h2>
+                    </NavLink>
+                    <NavLink to={'/skills'} className={classes.listItem}>
+                        <FontAwesomeIcon icon={faStickerMule}/>
+                        <h3>Skills</h3>
+                        </NavLink>
                     <NavLink to={'/projects'} className={classes.listItem}>My Portfolio</NavLink>
                     <NavLink to={'/Contacts'}className={classes.listItem}>Contact Me</NavLink>
                 </div>
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,28 +2,31 @@ import React from 'react';
 import classes from './Main.module.css'
 
 
+
+
 const Main = () => {
     return (
         <>
             <div className={classes.mainWrapper}>
-                <div className={classes.mainBlock}>
-                    <div className={classes.introduceBlock}>
-                        <span className={classes.introduceItemOne}>Hello!</span>
-                        <span className={classes.introduceItemTwo}>I'm Ilya Gab</span>
-                        <span className={classes.introduceItemThree}>Front-end Developer</span>
+                    <div className={classes.mainBlock}>
+                        <div className={classes.colorBlock}></div>
+                        <div className={classes.avatarBlock}></div>
                     </div>
-
-                    <div className={classes.avatarBlock}></div>
+                
+                    <div className={classes.introduceBlock}>
+                        <div>
+                        <h1 className={classes.introduceItemOne}>
+                            I'm
+                            Ilya
+                            Gab.
+                            <span className={classes.introduceItemTwo}>Front-end Developer</span>
+                        </h1>
+                        <p className={classes.introduceItemThree}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus accusamus ullam minus fuga, nulla nesciunt modi consequuntur autem delectus atque similique ipsum corporis beatae quisquam dolorum dolorem nihil debitis minima.</p>
+                    </div>
+                    </div>
                 </div>
-            </div>
-            <div className={classes.mainRemoteJobBlock}>
-                <div className={classes.mainRemoteJobBlockChild}>
-                    <span>Looking into remote work options</span>
-                    <button>Hire me</button>
-                </div>
-            </div>
-        </>
-    );
+            </>
+            );
 };
 
-export default Main;
+            export default Main;
